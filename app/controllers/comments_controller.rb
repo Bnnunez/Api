d3cc35c811155e3ee7ee6490a2b332c9a98620fa
Bnnunez <bnnunez@uc.cl>
2018-04-02 12:32:43 -0300

    Parche a pequeno error de requiere en controlador de comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -71,6 +71,6 @@ class CommentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def comment_params
-      params.require(:comment).permit(:comment, :author)
+      params.permit(:comment, :author)
     end
 end
