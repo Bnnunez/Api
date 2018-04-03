8ecd9151939f8f57bda6770f7e9a7f38a48bf2e5
Bnnunez <bnnunez@uc.cl>
2018-04-03 00:04:50 -0300

    Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts,except: 'update', path: 'news' do
-    put '', to: 'post#replace'
-    patch '', to: 'post#update'
+    put '', to: 'posts#replace'
+    patch '', to: 'posts#update'
     resources :comments,except: 'update' do
       put '', to: 'comments#replace'
       patch '', to: 'comments#update'
